9505b15ae617db7ca29cef97daeb5ca9262633d3
KudouMasaya <sohuto2kudou@yahoo.co.jp>
2017-10-22 18:38:29 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "水しるべ調査"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "ホーム | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "このサイトについて | #{@base_title}"
   end
 
 end
