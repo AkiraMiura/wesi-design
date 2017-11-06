da70188edbd0791ef8160ba0a6be8fdd9488b633
KudouMasaya <sohuto2kudou@yahoo.co.jp>
2017-11-06 12:25:04 +0000

    user delete ?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
   
   def index
     @users = User.all
@@ -42,10 +41,17 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
-
+    if current_user.admin?
+      User.find(params[:id]).destroy
+      flash[:success] = "管理者のあなたはユーザーを削除しました"
+      redirect_to users_url
+    elsif current_user?(@user)
+      flash[:success] = "あなたのアカウントは削除されました"
+      redirect_to root_path
+    else
+      flash[:success] = "?"
+      redirect_to root_path
+    end
   end
 
 
@@ -61,7 +67,7 @@ class UsersController < ApplicationController
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
-        flash[:danger] = "Please log in."
+        flash[:danger] = "ログインしてください."
         redirect_to login_url
       end
     end
