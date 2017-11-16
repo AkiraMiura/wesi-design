916eee9a0b9dbcec649b1c2170f5b7d500745df7
KudouMasaya <sohuto2kudou@yahoo.co.jp>
2017-11-16 02:48:54 +0000

    ログインの問題を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-  
-  has_many :surveys, dependent: :destroy
-  
+  #has_many :surveys, dependent: :destroy
   attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
@@ -33,6 +31,7 @@ class User < ApplicationRecord
   
     # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
